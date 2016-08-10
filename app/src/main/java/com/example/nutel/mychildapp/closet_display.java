fc9c49ad18482ba6f2a336dd22c2caaefaeea965
Flop3434 <benjamin.faulkingham@gmail.com>
2016-08-09 23:58:26 -0400

    Added some assets and messed around with the layouts

---
+++ app/src/main/java/com/example/nutel/mychildapp/closet_display.java
@@ -0,0 +1,16 @@
+package com.example.nutel.mychildapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Benjamin on 8/9/2016.
+ */
+public class closet_display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.closet_display);
+    }
+}
---
+++ app/src/main/java/com/example/nutel/mychildapp/event_display.java
@@ -0,0 +1,16 @@
+package com.example.nutel.mychildapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Benjamin on 8/9/2016.
+ */
+public class event_display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_display);
+    }
+}
