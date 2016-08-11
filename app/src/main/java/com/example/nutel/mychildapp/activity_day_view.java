71ce06178ec8e6cd61011008a19601ed6c5098df
Flop3434 <benjamin.faulkingham@gmail.com>
2016-08-10 21:59:42 -0400

    Added some assets and messed around with the layouts

--- app/src/main/java/com/example/nutel/mychildapp/activity_day_view.java
+++ app/src/main/java/com/example/nutel/mychildapp/activity_day_view.java
@@ -1,14 +1,39 @@
 package com.example.nutel.mychildapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class activity_day_view extends AppCompatActivity {
+
+    ImageButton closetimagebutton;
+    ImageButton alertimagebutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_day_view);
-    }
 
+        closetimagebutton = (ImageButton) findViewById(R.id.closetButton);
+        alertimagebutton = (ImageButton) findViewById(R.id.alertButton);
+
+        closetimagebutton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), closet_display.class));
+            }
+
+        });
+
+        alertimagebutton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), alert_display.class));
+            }
+        });
+
+
+    }
 }
--- app/src/main/java/com/example/nutel/mychildapp/event_activity.java
+++ app/src/main/java/com/example/nutel/mychildapp/event_activity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class closet_display extends AppCompatActivity {
+public class event_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_closet_display);
+        setContentView(R.layout.activity_event_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
