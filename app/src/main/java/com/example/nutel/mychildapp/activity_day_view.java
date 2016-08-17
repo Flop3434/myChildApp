8d937a65d476265dee2c1123f04e7db4cc9e645f
Flop3434 <benjamin.faulkingham@gmail.com>
2016-08-16 21:56:03 -0400

    Added the List view

--- app/src/main/java/com/example/nutel/mychildapp/activity_day_view.java
+++ app/src/main/java/com/example/nutel/mychildapp/activity_day_view.java
@@ -5,18 +5,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class activity_day_view extends AppCompatActivity {
+
+
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    String[] times = {"5am", "6am", "7am", "8am", "9am", "10am", "11 am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm",
+            "9pm", "10pm", "11pm", "12am", "1am", "2am", "3am", "4am",};
 
     ImageButton closetimagebutton;
     ImageButton alertimagebutton;
     ImageButton eventimagebutton;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_day_view);
+        listView = (ListView)findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, times);
+        listView.setAdapter(adapter);
+
+
+
 
         closetimagebutton = (ImageButton) findViewById(R.id.closetButton);
         alertimagebutton = (ImageButton) findViewById(R.id.alertButton);
