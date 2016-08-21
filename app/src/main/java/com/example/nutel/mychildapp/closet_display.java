60ed2d7b4ac8e30007c42116857ea7854af7ce9c
martin.emmanuel.santos <martin.emmanuel.santos@gmail.com>
2016-08-21 22:38:32 +1000

    - Added logic to -/+ buttons in "Closet" display - Added a date picker dialogue to the "Event" display

--- app/src/main/java/com/example/nutel/mychildapp/closet_display.java
+++ app/src/main/java/com/example/nutel/mychildapp/closet_display.java
@@ -2,16 +2,20 @@ package com.example.nutel.mychildapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class closet_display extends AppCompatActivity {
 
-    public Button backButton;
+    public Button buttonCancel,
+            buttonDecDiapers, buttonDecWipes, buttonDecOintments,
+            buttonIncDiapers, buttonIncWipes, buttonIncOintments;
+    public EditText textDiapers, textWipes, textOintments;
+    public int numDiapers, numWipes, numOintments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +24,86 @@ public class closet_display extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        backButton = (Button)findViewById(R.id.backButton);
+        // Create text fields
+        textDiapers = (EditText)findViewById(R.id.text_diapers);
+        textWipes = (EditText)findViewById(R.id.text_wipes);
+        textOintments = (EditText)findViewById(R.id.text_ointments);
 
-        backButton.setOnClickListener(new View.OnClickListener() {
+        // TODO: Read values from local database
+        numDiapers = 0;
+        numWipes = 0;
+        numOintments = 0;
+
+        textDiapers.setText(Integer.toString(numDiapers), TextView.BufferType.EDITABLE);
+        textWipes.setText(Integer.toString(numWipes), TextView.BufferType.EDITABLE);
+        textOintments.setText(Integer.toString(numOintments), TextView.BufferType.EDITABLE);
+
+        // Create subtract/add buttons
+        buttonDecDiapers = (Button)findViewById(R.id.button_dec_diapers);
+        buttonDecWipes = (Button)findViewById(R.id.button_dec_wipes);
+        buttonDecOintments = (Button)findViewById(R.id.button_dec_ointments);
+        buttonIncDiapers = (Button)findViewById(R.id.button_inc_diapers);
+        buttonIncWipes = (Button)findViewById(R.id.button_inc_wipes);
+        buttonIncOintments = (Button)findViewById(R.id.button_inc_ointments);
+
+        buttonDecDiapers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (numDiapers > 0) {
+                    numDiapers--;
+                    textDiapers.setText(Integer.toString(numDiapers), TextView.BufferType.EDITABLE);
+                }
+            }
+        });
+
+        buttonDecWipes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (numWipes > 0) {
+                    numWipes--;
+                    textWipes.setText(Integer.toString(numWipes), TextView.BufferType.EDITABLE);
+                }
+            }
+        });
+
+        buttonDecOintments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (numOintments > 0) {
+                    numOintments--;
+                    textOintments.setText(Integer.toString(numOintments), TextView.BufferType.EDITABLE);
+                }
+            }
+        });
+
+        buttonIncDiapers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numDiapers++;
+                textDiapers.setText(Integer.toString(numDiapers), TextView.BufferType.EDITABLE);
+            }
+        });
+
+        buttonIncWipes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numWipes++;
+                textWipes.setText(Integer.toString(numWipes), TextView.BufferType.EDITABLE);
+            }
+        });
+
+        buttonIncOintments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numOintments++;
+                textOintments.setText(Integer.toString(numOintments), TextView.BufferType.EDITABLE);
+            }
+        });
+
+        // Create Back button
+        buttonCancel = (Button)findViewById(R.id.backButton);
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(closet_display.this, activity_day_view.class));
--- app/src/main/java/com/example/nutel/mychildapp/event_display.java
+++ app/src/main/java/com/example/nutel/mychildapp/event_display.java
@@ -1,17 +1,17 @@
 package com.example.nutel.mychildapp;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -23,8 +23,8 @@ public class event_display extends AppCompatActivity {
 
     private static RadioGroup radioButtonGroup;
     private static RadioButton selectedRadioButton;
-    private static EditText TimeText;
-    public Button backButton;
+    private static EditText textDate, textTime;
+    public Button buttonCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +33,31 @@ public class event_display extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Set up the radio buttons for each event
-        radioButtonGroup = (RadioGroup)findViewById(R.id.radio_group_events);
+        // Create date picker dialogue
+        textDate = (EditText) findViewById((R.id.text_date));
+        textDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(v);
+            }
+        });
 
         // Create time picker dialogue
-        TimeText = (EditText) findViewById((R.id.text_time));
-        TimeText.setOnClickListener( new View.OnClickListener() {
+        textTime = (EditText) findViewById((R.id.text_time));
+        textTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showTimePickerDialog(v);
             }
         });
-        backButton = (Button)findViewById(R.id.button_cancel);
 
-        backButton.setOnClickListener(new View.OnClickListener() {
+        // Set up the radio buttons for each event
+        radioButtonGroup = (RadioGroup)findViewById(R.id.radio_group_events);
+
+        // Create Back button
+        buttonCancel = (Button)findViewById(R.id.button_cancel);
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(event_display.this, activity_day_view.class));
@@ -55,9 +66,33 @@ public class event_display extends AppCompatActivity {
 
     }
 
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
     public void showTimePickerDialog(View v) {
         DialogFragment newFragment = new TimePickerFragment();
         newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    // Class for selecting the date
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int day, int month) {
+            textDate.setText(month + "/" + day + "/" + year);
+        }
     }
 
     // Class for selecting the time
@@ -72,11 +107,9 @@ public class event_display extends AppCompatActivity {
             return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
         }
 
-
-
         @Override
         public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
-            TimeText.setText(String.format("%02d",hourOfDay) + ":" + String.format("%02d",minute));
+            textTime.setText(String.format("%02d",hourOfDay) + ":" + String.format("%02d",minute));
         }
 
     }
