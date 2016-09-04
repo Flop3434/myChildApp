a1b7f4df4081cc3c2c15b1443fc9fa5b4a5c5477
Flop3434 <benjamin.faulkingham@gmail.com>
2016-09-03 23:39:02 -0400

    Commited custom layout that does make sense because I suck

--- app/src/main/java/com/example/nutel/mychildapp/activity_day_view.java
+++ app/src/main/java/com/example/nutel/mychildapp/activity_day_view.java
@@ -16,8 +16,8 @@ public class activity_day_view extends AppCompatActivity {
 
     ListView listView;
     ArrayAdapter<String> adapter;
-    String[] times = {"5am", "6am", "7am", "8am", "9am", "10am", "11 am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm",
-            "9pm", "10pm", "11pm", "12am", "1am", "2am", "3am", "4am",};
+    String[] times = {"12am", "1am", "2am", "3am", "4am","5am", "6am", "7am", "8am", "9am", "10am", "11 am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm",
+            "9pm", "10pm", "11pm",};
 
     ImageButton closetimagebutton;
     ImageButton alertimagebutton;
